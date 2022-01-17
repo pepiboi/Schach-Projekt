6b3b80b3243020ae1d4a37072fec61090a66af1d
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-17 17:57:19 +0100

    onExitClickAction added

--- Schach-Programm/SchachFX/src/controller/BoardController.java
+++ Schach-Programm/SchachFX/src/controller/BoardController.java
@@ -2,11 +2,13 @@ package controller;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 public class BoardController {
     public GridPane boardId;
     public ImageView onClickExit;
+    public ColumnConstraints ABoardID;
 
     public void onMouseClickedExit(MouseEvent mouseEvent) {
         System.out.println("Programm wird geschlossen");
