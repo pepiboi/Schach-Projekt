8e5db1366eca25932691b1eb0577f07b524c5047
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-17 17:56:23 +0100

    made Chessboard bigger

--- Schach-Programm/SchachFX/src/controller/BoardController.java
+++ Schach-Programm/SchachFX/src/controller/BoardController.java
@@ -1,7 +1,15 @@
 package controller;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class BoardController {
     public GridPane boardId;
+    public ImageView onClickExit;
+
+    public void onMouseClickedExit(MouseEvent mouseEvent) {
+        System.out.println("Programm wird geschlossen");
+        System.exit(1);
+    }
 }
