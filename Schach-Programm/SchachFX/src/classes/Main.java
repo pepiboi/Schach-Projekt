c2e88c5cfe5dd0dab36b5bec8bd55c82fc557075
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-17 12:16:01 +0100

    Chessboard inizialized

--- Schach-Programm/SchachFX/src/classes/Main.java
+++ Schach-Programm/SchachFX/src/classes/Main.java
@@ -11,10 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("boardView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 900);
         primaryStage.setResizable(false);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Login");
         primaryStage.setScene(scene);
         primaryStage.show();
