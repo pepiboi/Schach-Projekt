27889f2d61a087e37894aa542291c6fddc25cd2a
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-22 11:24:08 +0100

    onAction set und Initializable erstellt

--- Schach-Programm/SchachFX/src/controller/LoginController.java
+++ Schach-Programm/SchachFX/src/controller/LoginController.java
@@ -1,4 +1,25 @@
 package controller;
 
-public class LoginController {
+import classes.Main;
+import com.sun.corba.se.spi.activation.Server;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void onClick(ActionEvent actionEvent) {
+
+    }
 }
