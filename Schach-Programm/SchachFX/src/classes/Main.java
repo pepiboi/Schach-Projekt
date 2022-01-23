c1b8dff37620e59ab0c9f51ed0e0b77cd59c3942
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-23 14:15:07 +0100

    merging complete, conflicts solved

--- Schach-Programm/SchachFX/src/classes/Main.java
+++ Schach-Programm/SchachFX/src/classes/Main.java
@@ -10,20 +10,12 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    public static Stage fxmlLoader;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-<<<<<<< HEAD
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginView.fxml"));
         fxmlLoader.setControllerFactory(call -> new LoginController(primaryStage));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-=======
         Server server = new Server();
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("boardView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1177, 1007);
->>>>>>> connectivity
-        primaryStage.setResizable(false);
         //primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setTitle("Game");
         primaryStage.setScene(scene);
