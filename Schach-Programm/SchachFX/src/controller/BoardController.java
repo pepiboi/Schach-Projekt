6816be8015946a3980f0b1be1d96fe334036f54d
KoglerD <koglerd190073@sus.htl-grieskirchen.at>
2022-01-24 12:14:05 +0100

    Id vergeben

--- Schach-Programm/SchachFX/src/controller/BoardController.java
+++ Schach-Programm/SchachFX/src/controller/BoardController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -11,9 +13,52 @@ public class BoardController {
     public ColumnConstraints ABoardID;
     public ColumnConstraints BBoardID;
     public ColumnConstraints CBoardID;
+    public ImageView blackKnightLeft;
+    public ImageView blackRookLeft;
+    public ImageView blackBishopLeft;
+    public ImageView blackQueen;
+    public ImageView blackKing;
+    public ImageView blackBishopRight;
+    public ImageView blackKnightRight;
+    public ImageView blackRookRight;
+    public ImageView blackPawnZero;
+    public ImageView blackPawnOne;
+    public ImageView blackPawnTwo;
+    public ImageView blackPawnThree;
+    public ImageView blackPawnFour;
+    public ImageView blackPawnFive;
+    public ImageView blackPawnSix;
+    public ImageView blackPawnSeven;
+    public ImageView whiteRookLeft;
+    public ImageView whiteKnightLeft;
+    public ImageView whiteBishopLeft;
+    public ImageView whiteQueen;
+    public ImageView whiteKing;
+    public ImageView whiteBishopRight;
+    public ImageView whiteKnightRight;
+    public ImageView whiteRookRight;
+    public ImageView whitePawnZero;
+    public ImageView whitePawnOne;
+    public ImageView whitePawnTwo;
+    public ImageView whitePawnThree;
+    public ImageView whitePawnFour;
+    public ImageView whitePawnFive;
+    public ImageView whitePawnSix;
+    public ImageView whitePawnSeven;
 
     public void onMouseClickedExit(MouseEvent mouseEvent) {
         System.out.println("Programm wird geschlossen");
         System.exit(1);
+    }
+
+    private void movePawn(){
+
+    }
+
+    public void onMouseClick(MouseEvent mouseEvent) {
+        Node selectedPane = (Node)mouseEvent.getSource();
+        if (selectedPane.getId().equals(whitePawnZero) || selectedPane.getId().equals(whitePawnOne) ||selectedPane.getId().equals(whitePawnTwo) ||selectedPane.getId().equals(whitePawnThree) ||selectedPane.getId().equals(whitePawnFour) ||selectedPane.getId().equals(whitePawnFive) ||selectedPane.getId().equals(whitePawnSix) ||selectedPane.getId().equals(whitePawnSeven)){
+
+        }
     }
 }
